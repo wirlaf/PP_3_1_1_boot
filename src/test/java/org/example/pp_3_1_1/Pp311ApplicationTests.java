06d18b8f56e064113a98f2960d8dfc9c0449462a
wirlaf <sergei.wirlaf@gmail.com>
2024-09-29 10:42:12 +0300

    spring init

---
+++ src/test/java/org/example/pp_3_1_1/Pp311ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.pp_3_1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp311ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
